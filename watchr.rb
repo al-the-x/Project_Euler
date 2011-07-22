379e9d02e4faa7279679312bd51b96b3d6521a27
David Rogers <david@ethos-development.com>
2011-07-21 23:22:12 -0400

    I decided to try my hand at problem 1 with PHP, too, so that meant modifying my watchr script to monitor PHP files... Might as well time execution while I'm in there.

--- watchr.rb
+++ watchr.rb
@@ -1,3 +1,7 @@
 watch(/problem_\d\/solution.py$/) do |matches|
-    system "clear && python #{matches[0]}"
+    system "clear && time python #{matches[0]}"
+end
+
+watch(/problem_\d\/solution.php$/) do |matches|
+    system "clear && time phpunit #{matches[0]}"
 end
